16f38a41511a1dff6fee5f94e9edcd9aacaee722
Alejandro Caballero <lava.caballero@gmail.com>
2017-05-11 20:45:58 -0500

    Tuned notifications killer

--- admin.php
+++ admin.php
@@ -41,7 +41,7 @@ header("Content-Type: text/html; charset=utf-8"); ?>
     
     <!-- This template -->
     <link rel="stylesheet" type="text/css" href="<?= $template->url ?>/media/styles~v1.0.1-2.css">
-    <link rel="stylesheet" type="text/css" href="<?= $template->url ?>/media/post_styles~v1.0.1-2.css">
+    <link rel="stylesheet" type="text/css" href="<?= $template->url ?>/media/post_styles~v1.0.2-1.css">
     <link rel="stylesheet" type="text/css" href="<?= $template->url ?>/media/colors~v1.0.1-5.css">
     
     <? if( $settings->get("engine.hide_left_sidebar_on_admin_pages") != "true" && $template->count_left_sidebar_groups() > 0 ): ?>
