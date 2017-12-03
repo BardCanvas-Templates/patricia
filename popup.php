3f79051435b7e05b41fa957f227401896e99b1f0
Alejandro Caballero <lava.caballero@gmail.com>
2017-12-03 09:43:25 -0600

    Added TinyMCE support

--- popup.php
+++ popup.php
@@ -66,6 +66,19 @@ header("Content-Type: text/html; charset=utf-8"); ?>
     
 </div><!-- /#body_wrapper -->
 
+<!-- These must be at the end of the document -->
+<script type="text/javascript" src="<?= $config->full_root_path ?>/lib/tinymce-4.6.3/tinymce.min.js"></script>
+<? $template->render_tinymce_additions(); ?>
+<script type="text/javascript" src="<?= $config->full_root_path ?>/media/init_tinymce~v<?=$config->engine_version?>.js"></script>
+<script type="text/javascript">
+    $(document).ready(function()
+    {
+        tinymce.init(tinymce_defaults);
+        tinymce.init(tinymce_full_defaults);
+        tinymce.init(tinymce_minimal_defaults);
+    });
+</script>
+
 <? internals::render(__FILE__); ?>
 
 </body>
